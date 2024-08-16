0d4571ff522fa710612dd0bbae5a8a31cbc0d99a
joaovivy <oliverjoao505@gmail.com>
2024-08-16 18:14:16 +0000

    Comandos iniciais de python.

---
+++ 1. Tipos primitivos/Tipagem dinamica.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("csl || clear") # Limpa o terminal.
+
+# Tipagem dinâmica.
+nome = "João"
+idade = 22
+altura = 1.65
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
---
+++ 1. Tipos primitivos/Tipagem estatica.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("cls || clear") # Limpa o terminal.
+
+#Tipagem estática.
+nome: str # cadeia
+idade: int # inteiro
+altura: float # real 
+
+nome = "João"
+idade = 18
+altura = 1.65
+
+print(f"Nome:  {nome}")
+print(f"idade:  {idade}")
+print(f"altura:  {altura}")
