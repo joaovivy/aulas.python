bcb5b8295487164a05f716e7a4294d6ffa6eba75
joaovivy <oliverjoao505@gmail.com>
2024-09-05 20:33:49 +0000

    Salvo "05/09/24"

---
+++ 8.Listas./0.2 Exercício.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls || clear")
+
+tabuada = 0
+def tabuada_numeros(n1, n2):
+    calculo = n1 * n2
+    return tabuada
+
+numero = int(input("Digite um número: "))
+
+for i in range(1,11):
+    tabuada = print(f"{numero} x {i} = {i * numero} ")
+
+tabuada = tabuada_numeros(numero, i)
